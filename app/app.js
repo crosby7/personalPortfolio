eee3701f783a43574d9ba72e253bb9abf05b49b8
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-09-05 20:05:50 -0400

    Multiple Updates
    Single Page Document,
    Typed moved to app.js,
    projects/about pages init,
    media queries -- need mobileNav

---
+++ app/app.js
@@ -0,0 +1,16 @@
+import {loadPage, hamburgerMenu} from "../services/services.js";
+
+function initializeListeners() {
+  $(window).on("hashchange", loadPage);
+  loadPage();
+
+  $("#hamburgerMenu").on("click", openHamburger);
+}
+
+$(document).ready(function () {
+  initializeListeners();
+});
+
+function openHamburger() {
+  hamburgerMenu.classList.toggle("openHamburger");
+}
---
+++ services/services.js
@@ -0,0 +1,38 @@
+let globalNav = document.getElementById("navLinks");
+export var hamburgerMenu = document.getElementById("hamburgerMenu");
+var typingEffect;
+
+export function loadPage() {
+    let hashTag = window.location.hash;
+    let pageToLoad = hashTag.replace("#", "");
+
+
+    if (pageToLoad != "")
+    {
+        $.get(`pages/${pageToLoad}.html`, function (data) {
+            $("#container").html(data);
+        })
+    }
+    else
+    {
+        $.get(`pages/home.html`, function (data) {
+            $("#container").html(data);
+            initializeTyped();
+        })
+    }
+
+    if (pageToLoad == "home")
+    {
+        initializeTyped();
+    }
+}
+
+function initializeTyped() {
+    typingEffect = new Typed(".multiText", {
+        strings : ["a coder", "a developer", "Cameron Crosby"],
+        loop : true,
+        typeSpeed : 100,
+        backSpeed : 80,
+        backDelay : 2000
+    });
+}
